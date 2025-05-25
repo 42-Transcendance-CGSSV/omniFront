3760295a457b54f5391ad2d1d5923e5cbe091f04
tchorniy <grodrigues991@gmail.com>
2025-05-25 14:34:48 +0200

    to school

--- classes/dcomponents/CanvaComponent.ts
+++ classes/dcomponents/CanvaComponent.ts
@@ -8,13 +8,42 @@ interface CanvaComponentProps extends AComponentProps {
 }
 
 export default class CanvaComponent extends AComponent<CanvaComponentProps> {
-	public render(): CanvaComponent {
-		this.element = document.createElement("canvas");
-		this.applyBasicProperties();
+	public element: HTMLCanvasElement | undefined = document.createElement("canvas");
+	public ctx!: CanvasRenderingContext2D;
 
-		if (this.props.onClick) {
-			this.addEventListener("click", this.props.onClick as EventListener);
+
+	public render(): CanvaComponent {
+		this.applyBasicProperties();
+		if (this.element) {
+			this.element.width = this.props.width;
+			this.element.height = this.props.height;
+			this.element.id = this.props.id;
 		}
+		
+		console.log("CanvaComponent render", this.element);
+
+		// if (this.props.onClick) {
+		// 	this.addEventListener("click", this.props.onClick as EventListener);
+		// }
 		return this;
+	}
+
+	public getElement(): HTMLElement | undefined {
+		if (!this.element) {
+			this.render();
+		}
+		console.log("CanvaComponent getElement", this.element);
+		return this.element;
+	}
+
+	public getContext(): CanvasRenderingContext2D {
+		if (this.element) {
+			this.ctx = (this.element as HTMLCanvasElement).getContext("2d") as CanvasRenderingContext2D;
+			if (!this.ctx) {
+				console.error("Failed to get 2D context");
+			}
+			return this.ctx;
+		}
+		throw new Error("Canvas element is not defined");
 	}
 }
