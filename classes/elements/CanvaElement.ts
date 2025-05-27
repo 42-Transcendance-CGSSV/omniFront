cd454cd9ece6b7987af98fa66c1b235b08266f3d
vSKAH <vskahhh@gmail.com>
2025-05-27 16:35:34 +0200

    add: adding event page-rendered. fest: fix canva rendering

--- classes/elements/CanvaElement.ts
+++ classes/elements/CanvaElement.ts
@@ -1,49 +1,36 @@
-import { AComponent, AComponentProps } from "./AComponent";
+import {AComponentProps, AElement} from "./AElement";
 
 interface CanvaComponentProps extends AComponentProps {
-	id: string;
-	width: number;
-	height: number;
-	onClick?: () => void;
+    id: string;
+    width: number;
+    height: number;
+    onClick?: () => void;
 }
 
-export default class CanvaComponent extends AComponent<CanvaComponentProps> {
-	public element: HTMLCanvasElement | undefined = document.createElement("canvas");
-	public ctx!: CanvasRenderingContext2D;
+export default class CanvaComponent extends AElement<CanvaComponentProps> {
+    public element: HTMLCanvasElement | undefined;
+    public ctx!: CanvasRenderingContext2D;
 
+    public render(): CanvaComponent {
+        this.element = document.createElement("canvas");
+        this.applyBasicProperties();
 
-	public render(): CanvaComponent {
-		this.applyBasicProperties();
-		if (this.element) {
-			this.element.width = this.props.width;
-			this.element.height = this.props.height;
-			this.element.id = this.props.id;
-		}
-		
-		console.log("CanvaComponent render", this.element);
+        if (this.element) {
+            this.element.width = this.props.width;
+            this.element.height = this.props.height;
+            this.element.id = this.props.id;
+        }
+        return this;
+    }
 
-		// if (this.props.onClick) {
-		// 	this.addEventListener("click", this.props.onClick as EventListener);
-		// }
-		return this;
-	}
-
-	public getElement(): HTMLElement | undefined {
-		if (!this.element) {
-			this.render();
-		}
-		console.log("CanvaComponent getElement", this.element);
-		return this.element;
-	}
-
-	public getContext(): CanvasRenderingContext2D {
-		if (this.element) {
-			this.ctx = (this.element as HTMLCanvasElement).getContext("2d") as CanvasRenderingContext2D;
-			if (!this.ctx) {
-				console.error("Failed to get 2D context");
-			}
-			return this.ctx;
-		}
-		throw new Error("Canvas element is not defined");
-	}
+    public getContext(): CanvasRenderingContext2D {
+        if (this.element) {
+            this.ctx = (this.element as HTMLCanvasElement).getContext("2d") as CanvasRenderingContext2D;
+            if (!this.ctx) {
+                console.error("Failed to get 2D context");
+            }
+            return this.ctx;
+        }
+        throw new Error("Canvas element is not defined");
+    }
 }
--- pages/PongPage.ts
+++ pages/PongPage.ts
@@ -1,58 +1,92 @@
-import {AComponent} from "@dcomponents/AComponent";
 import Page from "@classes/Page";
-import DivComponent from "@dcomponents/DivComponent";
-import TextComponent from "@dcomponents/TextComponent";
-import ButtonComponent from "@dcomponents/ButtonComponent";
 import NavBar from "@components/NavBar";
-import FeatureCard from "@components/FeatureCard";
-import GradientButton from "@components/GradientButton";
-import Footer from "@components/Footer";
-import axios from "axios";
-import CanvaComponent from "classes/dcomponents/CanvaComponent";
-import { c } from "node_modules/vite/dist/node/moduleRunnerTransport.d-DJ_mE5sf";
-let UPDATE_INTERVAL_MS = 17; // 60 FPS same as .env but fuck it 
+import CanvaComponent from "@elements/CanvaElement";
+
+let UPDATE_INTERVAL_MS = 17; // 60 FPS same as .env but fuck it
 
 let isInActiveinterval = false;
 const BASE = 'http://localhost:3000/api';
 const BASEAI = 'http://localhost:3001/api';
 let polling = false;
-const gameid = prompt("game id :")
+const gameid = 10;
+
 
 class PongPage extends Page {
-    private homeContainer   :CanvaComponent;
-    private ctx             :CanvasRenderingContext2D;
-    private player1ID       !:string;
-    private player2ID       !:string;
-    
-    
+    private canva: CanvaComponent;
+    private ctx: CanvasRenderingContext2D;
+    private player1ID       !: string;
+    private player2ID       !: string;
+
+
+    private readonly REFERENCE_WIDTH: number = 800;
+    private readonly REFERENCE_HEIGHT: number = 450;
+
+    private scale = 1;
+    private gameWidth = 0;
+    private gameHeight = 0;
+
+
     constructor() {
         super("PongPage", new NavBar({}));
-        this.homeContainer  = new CanvaComponent({id: "home-content", width: 800, height: 600, className: "w-[80%] h-[60%] sm:w-[60%] sm:h-[80%] sm:rotate-90 bg-black flex flex-col items-center justify-center p-4 overflow-hidden fixed inset-0 m-auto",});
-        this.ctx            = this.homeContainer.getContext();
-        const keysPressed   = new Set<string>();
-        
-        this.homeContainer.render();
-        
-        this.ctx?.canvas.setAttribute("tabindex", "0");
-        this.ctx?.canvas.addEventListener("keydown" , (event) => {
-            keysPressed.add(event.key);
+        this.canva = new CanvaComponent({
+            id: "pong-canva",
+            width: this.REFERENCE_WIDTH,
+            height: this.REFERENCE_HEIGHT,
+            className: "bg-black/50 p-4 m-auto",
+        });
+        this.addComponent(this.canva);
+        super.render();
+
+        const keysPressed = new Set<string>();
+        document.documentElement.addEventListener("page-rendered", () => {
+            this.setupResponsiveCanvas(this.canva)
+            this.ctx = this.canva.getContext();
+            this.ctx?.canvas.setAttribute("tabindex", "0");
+            this.ctx?.canvas.addEventListener("keydown", (event) => {
+                keysPressed.add(event.key);
+                if (this.ctx) {
+                    this.requestApiUpdate(keysPressed);
+                    // this.drawPaddles(this.ctx, this.player1pos, this.player2pos);
+                }
+            });
+            this.ctx?.canvas.addEventListener("keyup", (event) => {
+                keysPressed.delete(event.key);
+            });
+
             if (this.ctx) {
-                this.requestApiUpdate(keysPressed);
-                // this.drawPaddles(this.ctx, this.player1pos, this.player2pos);
+                this.getPositions();
             }
         });
-        this.ctx?.canvas.addEventListener("keyup" , (event) => {
-            keysPressed.delete(event.key);
-        });
-        
-        if (this.ctx) {
-            this.getPositions();
-        }
-        
-        this.addComponent(this.homeContainer);
     }
-    
-    private requestApiUpdate(keysPressed: Set<string>) : void {
+
+
+    private setupResponsiveCanvas(canvas: CanvaComponent): void {
+        const resize = () => {
+            const container = canvas.element?.parentElement!;
+            const containerWidth = container.clientWidth;
+            const containerHeight = container.clientHeight;
+
+            const widthRatio = containerWidth / this.REFERENCE_WIDTH;
+            const heightRatio = containerHeight / this.REFERENCE_HEIGHT;
+            this.scale = Math.min(widthRatio, heightRatio);
+
+            this.gameWidth = this.REFERENCE_WIDTH * this.scale;
+            console.log(`Game Width: ${this.gameWidth}`);
+            this.gameHeight = this.REFERENCE_HEIGHT * this.scale;
+
+            canvas.element!.width = this.gameWidth;
+            canvas.element!.height = this.gameHeight;
+            canvas.element!.style.width = `${this.gameWidth}px`;
+            canvas.element!.style.height = `${this.gameHeight}px`;
+            canvas.element!.style.display = 'block';
+            canvas.element!.style.margin = '0 auto';
+        };
+
+        resize();
+        window.addEventListener('resize', resize);
+    }
+
+    private requestApiUpdate(keysPressed: Set<string>): void {
         if (keysPressed.has("ArrowUp")) {
             this.moveUp(this.player2ID);
         }
@@ -73,108 +107,124 @@ class PongPage extends Page {
         }
     }
 
-    
-    private drawGame(data: { ball: { ballX: number; ballY: number; ballRadius: number }; players: { PlayerID: string; playerColor: string; side: string; PaddlePos: number }[] }): void {
+
+    private drawGame(data: {
+        ball: { ballX: number; ballY: number; ballRadius: number };
+        players: { PlayerID: string; playerColor: string; side: string; PaddlePos: number }[]
+    }): void {
         this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
-        
-        
+
+
         this.ctx.beginPath();
         this.ctx.arc(data.ball.ballX, data.ball.ballY, data.ball.ballRadius, 0, Math.PI * 2);
         this.ctx.fillStyle = "white";
         this.ctx.fill();
-        
+
         data.players.forEach(player => {
             if (player.side === "left") {
                 this.player1ID = player.PlayerID;
-            }
-            else {
+            } else {
                 this.player2ID = player.PlayerID;
             }
-            this.ctx.fillStyle = player.playerColor; 
+            this.ctx.fillStyle = player.playerColor;
             const paddleWidth = 10;
             const paddleHeight = 80;
             const x = player.side === "left" ? 0 : this.ctx.canvas.width - paddleWidth;
             this.ctx.fillRect(x, player.PaddlePos, paddleWidth, paddleHeight);
         });
     }
+
     private getPositions() {
-        if (polling) 
+        if (polling)
             return;
         polling = true;
-        
+
         const loop = () => {
             fetch(`${BASE}/match/${gameid}`)
-            .then(res => res.json())
-            .then(data => {
-                this.drawGame(data);
-                if (data.isRunning) {
-                  this.MoveAIs(data);
-                }
-                setTimeout(loop, 17); 
-            })
-            .catch(error => {
-                console.error("Error fetching game data:", error);
-            });
+                .then(res => res.json())
+                .then(data => {
+                    this.drawGame(data);
+                    if (data.isRunning) {
+                        this.MoveAIs(data);
+                    }
+                    setTimeout(loop, 17);
+                })
+                .catch(error => {
+                    console.error("Error fetching game data:", error);
+                });
         }
-        
-        loop(); 
+
+        loop();
     }
+
     private startGame() {
-        fetch(`${BASE}/startGame/${gameid}`, { method: 'PUT' })
+        fetch(`${BASE}/startGame/${gameid}`, {method: 'PUT'})
     }
+
     private stopGame() {
-        fetch(`${BASE}/stopGame/${gameid}`, { method: 'PUT' })
+        fetch(`${BASE}/stopGame/${gameid}`, {method: 'PUT'})
     }
+
     private moveUp(playerID: string) {
         fetch(`${BASE}/movePlayerUp/${playerID}`, {
-            method: 'POST' ,
-            headers : {'Content-Type': 'application/json'},
-            body: JSON.stringify({ playerID }),
-        })
-    }
-    
-    private moveDown(playerID: string) {  
-        fetch(`${BASE}/movePlayerDown/${playerID}`, { 
-            method: 'POST', 
+            method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({playerID}),
         })
     }
 
-    private async MoveAIs(data: { ball: { ballX: number; ballY: number; ballVelocityX: number; ballVelocityY: number }; players: { PlayerID: string; playerColor: string; side: string; PaddlePos: number; AI: boolean; PlayerName: string; numberOfGoals: number }[] }) {
+    private moveDown(playerID: string) {
+        fetch(`${BASE}/movePlayerDown/${playerID}`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({playerID}),
+        })
+    }
+
+    private async MoveAIs(data: {
+        ball: { ballX: number; ballY: number; ballVelocityX: number; ballVelocityY: number };
+        players: {
+            PlayerID: string;
+            playerColor: string;
+            side: string;
+            PaddlePos: number;
+            AI: boolean;
+            PlayerName: string;
+            numberOfGoals: number
+        }[]
+    }) {
         // console.log(data);
-      
-        data.players.forEach(async player =>{
-            if (player.AI === true){
-              let res = await fetch(`${BASEAI}/train`, {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                  playerID : player.PlayerName,
-                  ballX: data.ball.ballX,
-                  ballY: data.ball.ballY,
-                  ballSpeedX: data.ball.ballVelocityX,
-                  ballSpeedY: data.ball.ballVelocityY,
-                  myPosition: player.PaddlePos,
-                  PaddleHeight: '80',
-                  myScore:    player.numberOfGoals,
-                  mySide: `${player.side === 'left' ? 0.9 : 0.1}`
+
+        data.players.forEach(async player => {
+            if (player.AI === true) {
+                let res = await fetch(`${BASEAI}/train`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        playerID: player.PlayerName,
+                        ballX: data.ball.ballX,
+                        ballY: data.ball.ballY,
+                        ballSpeedX: data.ball.ballVelocityX,
+                        ballSpeedY: data.ball.ballVelocityY,
+                        myPosition: player.PaddlePos,
+                        PaddleHeight: '80',
+                        myScore: player.numberOfGoals,
+                        mySide: `${player.side === 'left' ? 0.9 : 0.1}`
+                    })
                 })
-              })
-              let readyRes = await res.json();
-              if (readyRes.move === 'up') {
-                // console.log(player.PlayerID);
-                this.moveUp(player.PlayerID);
-              }
-              else if (readyRes.move === 'down') {
-                this.moveDown(player.PlayerID);
-              }
+                let readyRes = await res.json();
+                if (readyRes.move === 'up') {
+                    // console.log(player.PlayerID);
+                    this.moveUp(player.PlayerID);
+                } else if (readyRes.move === 'down') {
+                    this.moveDown(player.PlayerID);
+                }
             }
-          })
-      
-      }
+        })
+
+    }
 }
 
 export default PongPage;
